ce3a040383159e22e524d21864ea87a6fc5c21b6
Marc Filipan <marc@mudsu.com>
2018-03-14 22:46:38 +0100

    Adjusted date parser

--- lib/trailblazer/finder/utils/parse.rb
+++ lib/trailblazer/finder/utils/parse.rb
@@ -7,14 +7,15 @@ module Trailblazer
       class Parse
         # Need a replacement for this
         def self.date(value)
-          return unless is_datetime(value)
+          return unless valid_date(value)
           Date.parse(value).strftime('%Y-%m-%d')
         rescue ArgumentError
           nil
         end
 
-        def self.is_datetime(d)
-          %w[Date Time DateTime Timezone].any? { |t| d.class.name == t }
+        def self.valid_date(d)
+          date_hash = Date._parse(d.to_s)
+          Date.valid_date?(date_hash[:year].to_i, date_hash[:mon].to_i, date_hash[:mday].to_i)
         end
 
         def self.term(value)
