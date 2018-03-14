4aa26e98e7b33cf59eb39b4ff6bae8c801547bfe
Marc Filipan <marc@mudsu.com>
2018-03-14 21:57:36 +0100

    Loop through specified default predicates

--- lib/trailblazer/finder/features/predicate.rb
+++ lib/trailblazer/finder/features/predicate.rb
@@ -2,6 +2,8 @@ module Trailblazer
   class Finder
     module Features
       module Predicate
+        PREDICATES = %w[eq not_eq blank not_blank lt lte gt gte].freeze
+
         def self.included(base)
           base.extend ClassMethods
           base.instance_eval do
@@ -20,14 +22,7 @@ module Trailblazer
 
           def predicates_for(*attributes)
             attributes.each do |attribute|
-              do_filters(attribute, "eq")
-              do_filters(attribute, "not_eq")
-              do_filters(attribute, "blank")
-              do_filters(attribute, "not_blank")
-              do_filters(attribute, "gt")
-              do_filters(attribute, "gte")
-              do_filters(attribute, "lt")
-              do_filters(attribute, "lte")
+              PREDICATES.each { |predicate| do_filters(attribute, predicate) }
             end
           end
         end
