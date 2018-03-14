6a5c12ad8a3573d2045d78b036267eb6222df412
Marc Filipan <marc@mudsu.com>
2018-03-15 00:04:57 +0100

    Shortened deep_locate

--- lib/trailblazer/finder/utils/deep_locate.rb
+++ lib/trailblazer/finder/utils/deep_locate.rb
@@ -2,24 +2,20 @@ module Trailblazer
   class Finder
     module Utils
       module DeepLocate
-        def self.deep_locate(comparator, object)
-          _deep_locate(comparator, object)
-        end
-
-        def self._deep_locate(comparator, object, result = [])
+        def self.deep_locate(comparator, object, result = [])
           if object.is_a?(::Enumerable)
-            if object.any? { |value| _match_comparator?(value, comparator, object) }
+            if object.any? { |value| match_comparator?(value, comparator, object) }
               result.push object
             end
             (object.respond_to?(:values) ? object.values : object.entries).each do |value|
-              _deep_locate(comparator, value, result)
+              deep_locate(comparator, value, result)
             end
           end
 
           result
         end
 
-        def self._match_comparator?(value, comparator, object)
+        def self.match_comparator?(value, comparator, object)
           if object.is_a?(::Hash)
             key, value = value
           else
