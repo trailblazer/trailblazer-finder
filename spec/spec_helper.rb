94c03d60e46cb2f3d2ebc2f890f08decffe39d2f
Abdelkader Boudih <terminale@gmail.com>
2019-08-15 01:32:59 +0200

    remove coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 require "bundler/setup"
-require "simplecov"
-SimpleCov.start do
-  add_group "Trailblazer-Finder", "lib"
-  add_group "Tests", "spec"
-end
 require "pry-byebug"
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :expect }
 end
-
-require "coveralls"
-Coveralls.wear!
 
 require "trailblazer/finder"
