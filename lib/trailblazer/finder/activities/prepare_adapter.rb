15116f1c741ec5e69010e618f68015295ca1cd02
Abdelkader Boudih <terminale@gmail.com>
2025-03-03 18:53:05 +0000

    fix build

--- lib/trailblazer/finder/activities/prepare_adapter.rb
+++ lib/trailblazer/finder/activities/prepare_adapter.rb
@@ -8,19 +8,18 @@ module Trailblazer
           ctx[:adapter] = ctx[:config].adapter
         end
 
-        def validate_adapter(_ctx, adapter:, **)
-          ORM_ADAPTERS.(adapter.to_s)
-        end
-
-        def invalid?(exception, (ctx, _flow_options), **_circuit_options)
-          (ctx[:errors] ||= []) << {adapter: "The specified adapter is invalid"}
-          true # Make sure to return true to indicate the handler handled the error
+        def validate_adapter(ctx, adapter:, **)
+          begin
+            ORM_ADAPTERS.(adapter.to_s)
+            true
+          rescue Dry::Types::ConstraintError
+            (ctx[:errors] ||= []) << {adapter: "The specified adapter is invalid"}
+            false
+          end
         end
 
         pass :set_adapter
-        step Rescue(Dry::Types::ConstraintError, handler: :invalid?) {
-          step :validate_adapter
-        }, Output(:failure) => End(:failure)
+        step :validate_adapter
       end
     end
   end
